8436dff0a35a9bdd77897c31cdbb1416110d62d2
Salahdin Rezk <s-salahdin.rezk@zewailcity.edu.eg>
2024-03-15 12:00:55 +0200

    Move draw shapes functions declarations from x,y to rx,ry

--- DrawShapes.h
+++ DrawShapes.h
@@ -3,8 +3,8 @@
 #include "CMUgraphicsLib\CMUgraphics.h"
 
 void DrawCar(window& w, color c, int rx, int ry, int size);
-void DrawCloud(window& w, color c, int x, int y, int size);
-void DrawHouse(window& w, color c, int x, int y, int size);
-void DrawIceCream(window& w, color c, int x, int y, int size);
-void DrawPlane(window& w, color c, int x, int y, int size);
-void DrawTree(window& w, color c, int x, int y, int size);
+void DrawCloud(window& w, color c, int rx, int ry, int size);
+void DrawHouse(window& w, color c, int rx, int ry, int size);
+void DrawIceCream(window& w, color c, int rx, int ry, int size);
+void DrawPlane(window& w, color c, int rx, int ry, int size);
+void DrawTree(window& w, color c, int rx, int ry, int size);
