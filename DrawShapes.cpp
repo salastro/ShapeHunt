e40b6ca8f0415fa0b811ab7bb507a9d47f211937
Salahdin Rezk <s-salahdin.rezk@zewailcity.edu.eg>
2024-03-15 12:16:12 +0200

    Change color variable names

--- DrawShapes.cpp
+++ DrawShapes.cpp
@@ -12,16 +12,16 @@ void DrawCar(window& w, color c, int rx, int ry, int size) {
 	w.DrawRectangle(rx, ry + size / 2, rx + size, ry + size * 7 / 8);
 
 	// Wheels color
-	color cWheels = color(85 - c.ucRed, 85 - c.ucGreen, 85 - c.ucBlue);
-	w.SetPen(cWheels); w.SetBrush(cWheels);
+	color wheelsColor = color(85 - c.ucRed, 85 - c.ucGreen, 85 - c.ucBlue);
+	w.SetPen(wheelsColor); w.SetBrush(wheelsColor);
 
 	// Draw wheels
 	w.DrawCircle(rx + size / 4, ry + size * 7 / 8, size / 8);
 	w.DrawCircle(rx + size * 3 / 4, ry + size * 7 / 8, size / 8);
 
 	// Top color
-	color cTop = color(255 - c.ucRed, 255 - c.ucGreen, 255 - c.ucBlue);
-	w.SetPen(cTop); w.SetBrush(cTop);
+	color topColor = color(255 - c.ucRed, 255 - c.ucGreen, 255 - c.ucBlue);
+	w.SetPen(topColor); w.SetBrush(topColor);
 
 	// Draw top: rectangle in the center and two triangles on the sides
 	w.DrawRectangle(rx + size / 4, ry + size / 4, rx + size * 3 / 4, ry + size / 2);
@@ -29,8 +29,8 @@ void DrawCar(window& w, color c, int rx, int ry, int size) {
 	w.DrawTriangle(rx + size * 3 / 4, ry + size / 4, rx + size, ry + size / 2, rx + size * 3 / 4, ry + size / 2);
 
 	// Windows color
-	color cWindows = color(170 - c.ucRed, 170 - c.ucGreen, 170 - c.ucBlue);
-	w.SetPen(cWindows); w.SetBrush(cWindows);
+	color windowsColor = color(170 - c.ucRed, 170 - c.ucGreen, 170 - c.ucBlue);
+	w.SetPen(windowsColor); w.SetBrush(windowsColor);
 
 	// Draw windows
 	w.DrawRectangle(rx + size / 8, ry + size * 2 / 5, rx + size * 3 / 8, ry + size * 4 / 6);
@@ -43,8 +43,8 @@ void DrawHouse(window& w, color c, int rx, int ry, int size) {
 	w.DrawRectangle(rx, ry + size / 3, rx + size, ry + size);
 
 	// Roof color
-	color cInverted = color(255 - c.ucRed, 255 - c.ucGreen, 255 - c.ucBlue);
-	w.SetPen(cInverted); w.SetBrush(cInverted);
+	color roofColor = color(255 - c.ucRed, 255 - c.ucGreen, 255 - c.ucBlue);
+	w.SetPen(roofColor); w.SetBrush(roofColor);
 
 	// Draw roof
 	w.DrawTriangle(rx, ry + size / 3, rx + size, ry + size / 3, rx + size / 2, ry);
@@ -54,7 +54,7 @@ void DrawCloud(window& w, color c, int rx, int ry, int size) {
 	w.SetPen(c); w.SetBrush(c);
 	// Draw cloud
 	w.DrawCircle(rx + size / 2, ry + size / 2, size / 4);
-	w.DrawCircle(rx + size - size/4, ry + size / 2, size / 4);
+	w.DrawCircle(rx + size - size / 4, ry + size / 2, size / 4);
 	w.DrawCircle(rx - size / 4 + size / 2, ry + size / 2, size / 4);
 	w.DrawCircle(rx + size / 8 + size / 2, ry - size / 8 + size / 2, size / 4);
 	w.DrawCircle(rx - size / 8 + size / 2, ry - size / 8 + size / 2, size / 4);
