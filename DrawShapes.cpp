181a252450bf9a7d777c2ef249de6c459f5010d8
Salahdin Rezk <s-salahdin.rezk@zewailcity.edu.eg>
2024-03-15 11:59:09 +0200

    Format code

--- DrawShapes.cpp
+++ DrawShapes.cpp
@@ -33,8 +33,8 @@ void DrawCar(window& w, color c, int rx, int ry, int size) {
 	w.SetPen(cWindows); w.SetBrush(cWindows);
 
 	// Draw windows
-	w.DrawRectangle(rx + size / 8, ry + size * 2/ 5, rx + size * 3 / 8, ry + size * 4 / 6);
-	w.DrawRectangle(rx + size * 5 / 8, ry + size * 2/ 5, rx + size * 7 / 8, ry + size * 4 / 6);
+	w.DrawRectangle(rx + size / 8, ry + size * 2 / 5, rx + size * 3 / 8, ry + size * 4 / 6);
+	w.DrawRectangle(rx + size * 5 / 8, ry + size * 2 / 5, rx + size * 7 / 8, ry + size * 4 / 6);
 }
 
 void DrawHouse(window& w, color c, int rx, int ry, int size) {
